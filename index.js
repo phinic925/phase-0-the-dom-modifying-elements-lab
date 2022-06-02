ed0c1f86fad7da0407400836b7cef3f22776798e
phinic925 <phinicnyangate480@gmail.com>
2022-06-02 16:35:18 +0100

    commit tree 2887855498388efc97d88b30e3c129770ad4ede0

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code here
+const main = document.getElementById("main");
+main.remove();
+const newHeader= document.createElement("h1");
+newHeader.id='victory';
+document.body.append(h1);
+newHeader.textContent = "YOUR-NAME is the champion";
